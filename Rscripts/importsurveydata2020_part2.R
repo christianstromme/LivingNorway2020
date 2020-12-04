91d9b60130fe57112ecac70b9d2762c627867f1a
Christian Bianchi Strømme <Christian.Stromme@uib.no>
2020-12-04 12:09:06 +0100

    Import scripts for all three parts, codes functional.

--- Rscripts/importsurveydata2020_part2.R
+++ Rscripts/importsurveydata2020_part2.R
@@ -1,5 +1,8 @@
 #### IMPORT DATA FROM PART 2 ####
+# load libraries
 
+library("tidyverse")
+library("wesanderson")
 
 # read in files
 data_raw2 <- read_csv2(file = "data/2dataset_clean.csv", skip_empty_rows = TRUE)
@@ -11,12 +14,12 @@ questions2 <- read_csv2(file = "data/2complete_clean.csv", n_max = 1, col_names 
 # clean and 
 data2 <- data_raw2 %>%
   # make long table
-  pivot_longer(cols = c(s_2,s_3, s_8,s_12, s_9:s_10_12), names_to = "Question2", values_to = "Value2") %>% 
+  pivot_longer(cols = c(s_2, s_3, s_8, s_12, s_9:s_10_12), names_to = "Question2", values_to = "Value2") %>% 
   
   # make categories
   mutate(Category = case_when(Question2 %in% c("s_2", "s_3") ~ "Teaching2",
                               Question2 %in% c("s_8", "s_12") ~ "Supervision2",
-                              Question2 %in% c("s_9":"s_10_12") ~ "Own_education")) %>% 
+                              Question2 %in% c("s_9", "s_10_1", "s_10_2","s_10_3","s_10_4", "s_10_5", "s_10_6", "s_10_7", "s_10_8", "s_10_9", "s_10_10","s_10_11", "s_10_12") ~ "Own_education")) %>% 
   
   # rename questions
   mutate(Question2 = case_when(Question2 == "s_2" ~ "Teaches",
