af729e62d622e2ace45a790b80f937080522f2d6
audhalbritter <me@audhalbritter.com>
2021-06-21 16:28:17 +0200

    prettify results

--- Rscripts/results_plan.R
+++ Rscripts/results_plan.R
@@ -8,30 +8,60 @@ results_plan = drake_plan(
     global = tidy(eng.clmm.g),
     final = tidy(eng.clmm.f),
     .id = "model"
-  ),
+  ) %>% 
+    filter(coef.type == "location") %>% 
+    mutate(term = case_when(term == "University1" ~ "University",
+                            term == "University1:YearLate" ~ "University:YearLate",
+                            term == "GenderMale"~ "Male",
+                            TRUE ~ term),
+           term = case_when(p.value < 0.001 ~ paste(term, "***"),
+                               p.value < 0.01 ~ paste(term, "**"),
+                               p.value < 0.05 ~ paste(term, "*"),
+                               TRUE ~ term)) %>% 
+    select(model, `Fixed effect terms` = term, Estimate = estimate, SE = std.error, `z value` = statistic),
 
   
   ## Questions 1.2
   
-  result_Q1.2 = bind_rows(global_data_sharing = tidy(imp.clmm.a.g),
-            final_data_sharing = tidy(imp.clmm.a.f),
+  result_Q1.2 = bind_rows(global_data.sharing = tidy(imp.clmm.a.g),
+            final_data.sharing = tidy(imp.clmm.a.f),
             
-            global_code_sharing = tidy(imp.clmm.b.g),
-            final_code_sharing = tidy(imp.clmm.b.f),
+            global_code.sharing = tidy(imp.clmm.b.g),
+            final_code.sharing = tidy(imp.clmm.b.f),
             
-            global_method_method = tidy(imp.clmm.c.g),
-            final_method_method = tidy(imp.clmm.c.f),
+            global_method.method = tidy(imp.clmm.c.g),
+            final_method.method = tidy(imp.clmm.c.f),
             
-            global_open_publish = tidy(imp.clmm.d.g),
-            final_opne_publish = tidy(imp.clmm.d.f),
+            global_open.publish = tidy(imp.clmm.d.g),
+            final_open.publish = tidy(imp.clmm.d.f),
             .id = "model_aspect"
-            ),
+            ) %>% 
+    filter(coef.type == "location") %>% 
+    separate(col = model_aspect, into = c("model", "aspect"), sep = "_") %>% 
+    mutate(term = case_when(term == "DomainT" ~ "Teaching",
+                            term == "GenderMale"~ "Male",
+                            TRUE ~ term),
+           term = case_when(p.value < 0.001 ~ paste(term, "***"),
+                            p.value < 0.01 ~ paste(term, "**"),
+                            p.value < 0.05 ~ paste(term, "*"),
+                            TRUE ~ term)) %>% 
+    select(model, aspect, `Fixed effect terms` = term, Estimate = estimate, SE = std.error, `z value` = statistic),
   
   result_Q1.3 = bind_rows(
     global = tidy(use.clmm.g),
     final = tidy(use.clmm.f),
     .id = "model"
-  ),
+  ) %>% 
+    filter(coef.type == "location") %>% 
+    mutate(term = case_when(term == "Actionuse" ~ "Use",
+                            term == "AspectData" ~ "Data",
+                            term == "GenderMale"~ "Male",
+                            TRUE ~ term),
+           term = case_when(p.value < 0.001 ~ paste(term, "***"),
+                            p.value < 0.01 ~ paste(term, "**"),
+                            p.value < 0.05 ~ paste(term, "*"),
+                            TRUE ~ term)) %>% 
+    select(model, `Fixed effect terms` = term, Estimate = estimate, SE = std.error, `z value` = statistic),
   
   
   result_Q1.4 = bind_rows(
@@ -40,7 +70,16 @@ results_plan = drake_plan(
     global_supervise = tidy(supervise.glm.g),
     final_supervise = tidy(supervise.glm.f),
     .id = "model_domain"
-  ),
+  ) %>% 
+    separate(col = model_domain, into = c("model", "domain"), sep = "_") %>% 
+    mutate(term = case_when(term == "(Intercept)" ~ "Intercept",
+                            term == "GenderMale"~ "Male",
+                            TRUE ~ term),
+           term = case_when(p.value < 0.001 ~ paste(term, "***"),
+                            p.value < 0.01 ~ paste(term, "**"),
+                            p.value < 0.05 ~ paste(term, "*"),
+                            TRUE ~ term)) %>%
+    select(Model = model, Domain = domain, `Fixed effect terms` = term, Estimate = estimate, SE = std.error, `z value` = statistic),
   
   
   result_Q1.4.2 = bind_rows(global_teach_data = tidy(teach.clm.data.g),
@@ -61,7 +100,8 @@ results_plan = drake_plan(
                             global_supervise_publish = tidy(superv.clm.publish.g),
                             
                             .id = "model_domain_aspect"
-  ),
+  ) %>% 
+    filter(coef.type == "location"),
   
   
   result_Q3.2 = bind_rows(global_use_data = tidy(learn.clm.a.g),
@@ -92,14 +132,16 @@ results_plan = drake_plan(
                           final_do_outreach = tidy(learn.clm.i.f),
                           
                           .id = "model_action_aspect"
-  ),
+  ) %>% 
+    filter(coef.type == "location"),
   
   
   result_Q3.3 = bind_rows(
     global_R_vs_T = tidy(imp.clmm.g),
     final_R_vs_T = tidy(imp.clmm.f),
     .id = "model_domain"
-  )
+  ) %>% 
+    filter(coef.type == "location")
 
   
 )
